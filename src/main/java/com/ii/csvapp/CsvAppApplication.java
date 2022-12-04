19b8b065024f1bd9472e6dc27c2f94614c12b869
VENKATASAIREDDY <venkatasai1478@gmail.com>
2022-12-04 10:53:14 +0530

    initial changes

---
+++ src/main/java/com/ii/csvapp/CsvAppApplication.java
@@ -0,0 +1,13 @@
+package com.ii.csvapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ii/csvapp/CsvAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ii.csvapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
